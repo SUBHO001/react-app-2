52561556e9a673820dc9cca2d50aa34a91a2e34f
SUBHO001 <subhankar.sarkar.001@gmail.com>
2021-05-06 15:07:55 +0530

    completed to 480

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupsPage from './pages/NewMeetups';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   //localhost:3000/
   //my-page.com/
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path='/' exact>
           <AllMeetupsPage />
@@ -22,7 +21,7 @@ function App() {
           <NewMeetupsPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,5 +1,29 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import { useHistory } from 'react-router-dom';
+
 function NewMeetupsPage() {
-  return <div>NewMeetupsPage</div>;
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-dd63c-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'applicaion/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/'); // can use push but replace does not let back work
+    });
+  }
+  return (
+    <section>
+      <h1>New Meetup Form </h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
